11349f74ee376e56e0cd320947e8bbbe187c2668
FELIX FRANCISCO <felix_francisco@estudante.sesisenai.org.br>
2022-08-12 09:08:46 -0300

    tela inicial

--- app/src/main/java/com/example/controlecigarro/Inicial.java
+++ app/src/main/java/com/example/controlecigarro/Inicial.java
@@ -3,12 +3,21 @@ package com.example.controlecigarro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Inicial extends AppCompatActivity {
-
+    Button charuto,cigarro,vape,narguilé,cachimbo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicial);
+        getSupportActionBar().hide();
+        cigarro=findViewById(R.id.cigarro);
+        charuto=findViewById(R.id.charuto);
+        vape=findViewById(R.id.vape);
+        narguilé=findViewById(R.id.narguile);
+        cachimbo=findViewById(R.id.cachimbo);
+
     }
 }
